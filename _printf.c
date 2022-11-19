3ea45aaa7bb75dc5c5023fd1367cc3987d8f70ec
Favour <divinephavor@gmail.com>
2022-11-19 12:25:59 -0800

    Task 0

--- _printf.c
+++ _printf.c
@@ -7,7 +7,6 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -58,7 +57,6 @@ int _printf(const char *format, ...)
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**PRINT CHAR**/
+/***PRINT CHAR***/
 
 /**
  * print_char - Prints a char
@@ -21,7 +21,7 @@ int print_char(va_list types, char buffer[],
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
 
-/**PRINT A STRING**/
+/***PRINT A STRING***/
 
 /**
  * print_string - Prints a string
@@ -79,8 +79,7 @@ int print_string(va_list types, char buffer[],
 	return (write(1, str, length));
 }
 
-/**PRINT PERCENT SIGN**/
-
+/***PRINT %***/
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -105,7 +104,6 @@ int print_percent(va_list types, char buffer[],
 }
 
 /***PRINT INT***/
-
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -150,7 +148,7 @@ int print_int(va_list types, char buffer[],
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
-/*PRINT BINARY*/
+/***PRINT BINARY***/
 
 /**
  * print_binary - Prints an unsigned number
